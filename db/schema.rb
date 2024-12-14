c8df4b190fe6f4b697cc2946cb0c61c9f0d25f1b
wayo848yowa <142649823+wayo848yowa@users.noreply.github.com>
2024-12-14 06:48:01 +0000

    いったん　入力フォームだけ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_14_015115) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_14_060917) do
   create_table "comments", force: :cascade do |t|
     t.string "user_name"
     t.text "body"
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_14_015115) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
   add_foreign_key "comments", "ideas"
